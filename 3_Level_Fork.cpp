9264fde2c492065a72f54ec41ccf56b02455e1e9
Cann-E <cann0211@gmail.com>
2025-02-08 22:42:59 -0600

    initial commit

--- 3_Level_Fork.cpp
+++ 3_Level_Fork.cpp
@@ -5,11 +5,11 @@
 int main()
 {
     std::cout << "I am the parent process" << std::endl;
-    for(int i=0;i<3;i++){
+    for(int i=0;i<4;i++){
         if (fork()==0){
             std::cout << "I am the child process " << i << std::endl;
-            if(i==2){
-                for(int j=0;j<4;j++){
+            if(i==3){
+                for(int j=0;j<3;j++){
                     if(fork()==0){
                         std::cout << "I am a grandchild process from child process " << i << std::endl;
                         _exit(0);
@@ -17,28 +17,20 @@ int main()
                     wait(nullptr);
                 }
             }
-            if(i==1){
-                for(int j=0;j<3;j++){
-                    if (fork()==0){
-                        std::cout << "I am a grandchild process from child process " << i << std::endl;
-                        _exit(0);
-                    }
-                    wait(nullptr);
-                }
-            }
-            if(i==0){
+            if(i==2){
                 for(int j=0;j<2;j++){
                     if(fork()==0){
                         std::cout << "I am a grandchild process from child process " << i << std::endl;
-                        if(j==0){
-                            for(int z=0;z<1;z++){
-                                if (fork()==0){
-                                    std::cout<<"I am the great-grandchild process from the grandchild process "<<j<<std::endl;
-                                    _exit(0);
-                                }
-                                wait(nullptr);
-                            }
-                        }
+                        _exit(0);
+                    }
+                    wait(nullptr);
+                }
+            }
+            if (i==1){
+                for(int j=0;j<1;j++){
+                    if (fork()==0){
+                        std::cout << "I am a grandchild process from child process " << i << std::endl;
+                        _exit(0);
                     }
                     wait(nullptr);
                 }
@@ -48,21 +40,21 @@ int main()
         wait(nullptr);
     }
  
+ 
+  // 
+  // 
     return 0;
 }
 
 
 // I am the parent process
 // I am the child process 0
-// I am a grandchild process from child process 0
-// I am the great-grandchild process from the grandchild process 0
-// I am a grandchild process from child process 0
 // I am the child process 1
-// I am a grandchild process from child process 1
-// I am a grandchild process from child process 1
 // I am a grandchild process from child process 1
 // I am the child process 2
 // I am a grandchild process from child process 2
 // I am a grandchild process from child process 2
-// I am a grandchild process from child process 2
-// I am a grandchild process from child process 2
+// I am the child process 3
+// I am a grandchild process from child process 3
+// I am a grandchild process from child process 3
+// I am a grandchild process from child process 3
