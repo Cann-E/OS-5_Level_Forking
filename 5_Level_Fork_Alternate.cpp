998692fd11082e163de78613e441bb2c7fc29e04
Cann-E <cann0211@gmail.com>
2025-02-09 13:19:36 -0600

    initial commit

---
+++ 5_Level_Fork_Alternate.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    std::cout << "I am the parent process" << std::endl;
+    
+    for(int i=0; i<2; i++){
+        if(fork()==0){
+            std::cout << "I am the child process " << i << std::endl;
+
+            if(i==1){
+                for(int j=0; j<2; j++){
+                    if(fork()==0){
+                        std::cout << "I am a grandchild process from child process " << i << std::endl;
+                        
+                        if(j==1){
+                            for(int z=0; z<2; z++){
+                                if(fork()==0){
+                                    std::cout << "I am a great-grandchild process from child process " << j << std::endl;
+                                    
+                                    for(int x=0; x<2; x++){
+                                        if(fork()==0){
+                                            std::cout << "I am a great-great-grandchild process from child process " << x << std::endl;
+                                            _exit(0);
+                                        }
+                                    }
+                                    while(wait(nullptr) > 0);
+                                    _exit(0);
+                                }
+                            }
+                            while(wait(nullptr) > 0);
+                        }
+
+                        if(j==0){
+                            for(int z=0; z<2; z++){
+                                if(fork()==0){
+                                    std::cout << "I am a great-grandchild process from child process " << j << std::endl;
+                                    
+                                    for(int x=0; x<2; x++){
+                                        if(fork()==0){
+                                            std::cout << "I am a great-great-grandchild process from child process " << x << std::endl;
+                                            _exit(0);
+                                        }
+                                    }
+                                    while(wait(nullptr) > 0);
+                                    _exit(0);
+                                }
+                            }
+                            while(wait(nullptr) > 0);
+                        }
+                        _exit(0);
+                    }
+                }
+                while(wait(nullptr) > 0);
+            }
+
+            if(i==0){
+                for(int j=0; j<2; j++){
+                    if(fork()==0){
+                        std::cout << "I am a grandchild process from child process " << i << std::endl;
+
+                        for(int z=0; z<2; z++){
+                            if(fork()==0){
+                                std::cout << "I am a great-grandchild process from child process " << j << std::endl;
+
+                                for(int x=0; x<2; x++){
+                                    if(fork()==0){
+                                        std::cout << "I am a great-great-grandchild process from child process " << x << std::endl;
+                                        _exit(0);
+                                    }
+                                }
+                                while(wait(nullptr) > 0);
+                                _exit(0);
+                            }
+                        }
+                        while(wait(nullptr) > 0);
+                        _exit(0);
+                    }
+                }
+                while(wait(nullptr) > 0);
+            }
+            _exit(0);
+        }
+    }
+
+    while(wait(nullptr) > 0);
+    return 0;
+}
